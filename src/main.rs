26e3d43f85fa49929277361ad7f2653acc9ba16b
Lobby Divinus <fe-software@gmx.net>
2018-04-06 10:35:20 +0200

    Add support for text output and fix clear_area

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 #![no_std]
 #![no_main]
 #![feature(compiler_builtins_lib)]
+#![feature(alloc)]
 
-extern crate stm32f7_discovery as stm32f7;
+extern crate alloc;
 extern crate compiler_builtins;
 extern crate r0;
+extern crate stm32f7_discovery as stm32f7;
 
-use stm32f7::{system_clock, board, embedded, lcd, sdram, touch, i2c};
-use core::{fmt::{Write},ptr};
+use alloc::String;
+use alloc::string::ToString;
+use core::{ptr, fmt::Write};
 use stm32f7::lcd::font::FontRenderer;
-
-
+use stm32f7::{board, embedded, lcd, sdram, system_clock, touch, i2c};
 
 const WIDTH: i32 = 480;
 const HEIGHT: i32 = 272;
@@ -21,39 +23,41 @@ static TTF: &[u8] = include_bytes!("../RobotoMono-Bold.ttf");
 
 struct Point {
     x: i16,
-    y: i16
+    y: i16,
 }
 
 impl Copy for Point {}
 impl Clone for Point {
     fn clone(&self) -> Point {
-        Point { x: self.x, y: self.y}
+        Point {
+            x: self.x,
+            y: self.y,
+        }
     }
 }
 
-
 struct Renderer {
     bg_color: lcd::Color,
-    pixel_markers: [bool;(WIDTH * HEIGHT) as usize],
-    drawn_pixels: [Point;2 * PIXEL_BUFFER_SIZE],
-    drawn_pixel_count: [usize;2],
-    current_buffer:u8,
+    pixel_markers: [bool; (WIDTH * HEIGHT) as usize],
+    drawn_pixels: [Point; 2 * PIXEL_BUFFER_SIZE],
+    drawn_pixel_count: [usize; 2],
+    current_buffer: u8,
     layer: lcd::Layer<lcd::FramebufferArgb8888>,
     direct: bool,
-    frame_counter: i32
+    frame_counter: i32,
 }
 
 impl Renderer {
     fn new(l: lcd::Layer<lcd::FramebufferArgb8888>, background: lcd::Color) -> Renderer {
         Renderer {
             bg_color: background,
-            pixel_markers: [false;(WIDTH * HEIGHT) as usize],
-            drawn_pixels: [Point { x: 0, y: 0 };2 * PIXEL_BUFFER_SIZE],
-            drawn_pixel_count: [0;2],
+            pixel_markers: [false; (WIDTH * HEIGHT) as usize],
+            drawn_pixels: [Point { x: 0, y: 0 }; 2 * PIXEL_BUFFER_SIZE],
+            drawn_pixel_count: [0; 2],
             current_buffer: 0,
             layer: l,
             direct: true,
-            frame_counter: 0
+            frame_counter: 0,
         }
     }
 
@@ -63,7 +67,8 @@ impl Renderer {
         }
 
         if self.direct {
-            self.layer.print_point_color_at(x as usize, y as usize, color);
+            self.layer
+                .print_point_color_at(x as usize, y as usize, color);
         } else {
             self.pixel_markers[(x + y * WIDTH) as usize] = true;
             let offset = self.current_buffer as usize * PIXEL_BUFFER_SIZE;
@@ -72,7 +77,8 @@ impl Renderer {
             self.drawn_pixels[index].y = y as i16;
 
             let pixel = self.drawn_pixels[index];
-            self.layer.print_point_color_at(pixel.x as usize, pixel.y as usize, color);
+            self.layer
+                .print_point_color_at(pixel.x as usize, pixel.y as usize, color);
 
             self.drawn_pixel_count[self.current_buffer as usize] += 1;
         }
@@ -102,7 +108,8 @@ impl Renderer {
             let y = pixel.y as i32;
             if !self.pixel_markers[(x + y * WIDTH) as usize] {
                 let color = self.get_background(x, y);
-                self.layer.print_point_color_at(x as usize, y as usize, color);
+                self.layer
+                    .print_point_color_at(x as usize, y as usize, color);
             }
         }
 
@@ -120,7 +127,8 @@ impl Renderer {
                 let x = pixel.x as i32;
                 let y = pixel.y as i32;
                 let color = self.get_background(x, y);
-                self.layer.print_point_color_at(x as usize, y as usize, color);
+                self.layer
+                    .print_point_color_at(x as usize, y as usize, color);
                 self.pixel_markers[(x + y * WIDTH) as usize] = false;
             }
             self.drawn_pixel_count[buf as usize] = 0;
@@ -132,17 +140,19 @@ impl Renderer {
         for y in 0..HEIGHT {
             for x in 0..WIDTH {
                 let color = self.get_background(x, y);
-                self.layer.print_point_color_at(x as usize, y as usize, color);
+                self.layer
+                    .print_point_color_at(x as usize, y as usize, color);
             }
         }
     }
 
     fn clear_area(&mut self, x: i32, y: i32, w: i32, h: i32) {
         self.flush();
-        for py in x..x + w {
-            for px in y..y + h {
+        for py in y..y + h {
+            for px in x..x + w {
                 let color = self.get_background(px, py);
-                self.layer.print_point_color_at(px as usize, py as usize, color);
+                self.layer
+                    .print_point_color_at(px as usize, py as usize, color);
             }
         }
     }
@@ -151,9 +161,6 @@ impl Renderer {
         lcd::Color::rgb((WIDTH / 8 - x / 8) as u8, 0, (x / 8) as u8)
     }
 }
-
-
-
 
 #[no_mangle]
 pub unsafe extern "C" fn reset() -> ! {
@@ -195,9 +202,17 @@ fn main(hw: board::Hardware) -> ! {
         flash,
         fmc,
         ltdc,
-        gpio_a, gpio_b, gpio_c, gpio_d,
-        gpio_e, gpio_f, gpio_g, gpio_h,
-        gpio_i, gpio_j, gpio_k,
+        gpio_a,
+        gpio_b,
+        gpio_c,
+        gpio_d,
+        gpio_e,
+        gpio_f,
+        gpio_g,
+        gpio_h,
+        gpio_i,
+        gpio_j,
+        gpio_k,
         i2c_3,
         sai_2,
         syscfg,
@@ -208,27 +223,36 @@ fn main(hw: board::Hardware) -> ! {
         ..
     } = hw;
 
-    let mut gpio = Gpio::new(gpio_a, gpio_b, gpio_c, gpio_d,
-                             gpio_e, gpio_f, gpio_g, gpio_h,
-                             gpio_i, gpio_j, gpio_k);
+    let mut gpio = Gpio::new(
+        gpio_a,
+        gpio_b,
+        gpio_c,
+        gpio_d,
+        gpio_e,
+        gpio_f,
+        gpio_g,
+        gpio_h,
+        gpio_i,
+        gpio_j,
+        gpio_k,
+    );
 
     system_clock::init(rcc, pwr, flash);
 
     // enable all gpio ports
-    rcc.ahb1enr
-        .update(|r| {
-            r.set_gpioaen(true);
-            r.set_gpioben(true);
-            r.set_gpiocen(true);
-            r.set_gpioden(true);
-            r.set_gpioeen(true);
-            r.set_gpiofen(true);
-            r.set_gpiogen(true);
-            r.set_gpiohen(true);
-            r.set_gpioien(true);
-            r.set_gpiojen(true);
-            r.set_gpioken(true);
-        });
+    rcc.ahb1enr.update(|r| {
+        r.set_gpioaen(true);
+        r.set_gpioben(true);
+        r.set_gpiocen(true);
+        r.set_gpioden(true);
+        r.set_gpioeen(true);
+        r.set_gpiofen(true);
+        r.set_gpiogen(true);
+        r.set_gpiohen(true);
+        r.set_gpioien(true);
+        r.set_gpiojen(true);
+        r.set_gpioken(true);
+    });
 
     // init sdram (needed for display buffer)
     sdram::init(rcc, fmc, &mut gpio);
@@ -252,10 +276,7 @@ fn main(hw: board::Hardware) -> ! {
 
     touch::check_family_id(&mut i2c_3).unwrap();
 
-
-
     let mut renderer = Renderer::new(layer_1, bg_color);
-
 
     loop {
         game(&mut renderer, &mut i2c_3);
@@ -281,11 +302,17 @@ fn game(renderer: &mut Renderer, i2c_3: &mut i2c::I2C) {
     let mut v = 1f32;
     block_color = hsv_color(h, s, v);
 
-    draw_block(renderer, 0, last_block_start - 1, block_height, last_block_width, block_color);
+    draw_block(
+        renderer,
+        0,
+        last_block_start - 1,
+        block_height,
+        last_block_width,
+        block_color,
+    );
 
     block_color = hsv_color(h, s, v);
 
-    
     let font_renderer = FontRenderer::new(TTF, 14.0);
 
     loop {
@@ -303,9 +330,15 @@ fn game(renderer: &mut Renderer, i2c_3: &mut i2c::I2C) {
         p = -2f32 * p * p * p + 3f32 * p * p;
         let mut block_start = range_start + (p * range_width as f32) as i32;
 
-        draw_block(renderer, cur_stack_height, block_start, block_height, block_width, block_color);
-        
-        
+        draw_block(
+            renderer,
+            cur_stack_height,
+            block_start,
+            block_height,
+            block_width,
+            block_color,
+        );
+
         // poll for new touch data
         let mut tapped = false;
         for touch in &touch::touches(i2c_3).unwrap() {
@@ -313,8 +346,6 @@ fn game(renderer: &mut Renderer, i2c_3: &mut i2c::I2C) {
         }
 
         renderer.end_frame();
-
-        
 
         if tapped && !last_tapped {
             renderer.flush();
@@ -325,8 +356,15 @@ fn game(renderer: &mut Renderer, i2c_3: &mut i2c::I2C) {
             if block_start + block_width > last_block_start + last_block_width {
                 block_width -= block_start + block_width - last_block_start - last_block_width;
             }
-            draw_block(renderer, cur_stack_height - 1, block_start, block_height + 1, block_width, block_color);
-            
+            draw_block(
+                renderer,
+                cur_stack_height - 1,
+                block_start,
+                block_height + 1,
+                block_width,
+                block_color,
+            );
+
             last_block_start = block_start;
             last_block_width = block_width;
             cur_stack_height += block_height;
@@ -339,31 +377,27 @@ fn game(renderer: &mut Renderer, i2c_3: &mut i2c::I2C) {
             }
 
             renderer.clear_area(WIDTH - 20, 0, 20, HEIGHT);
-            let mut text = "Points: ";
-            //text.push_str(cur_stack_height.to_string());
-            font_renderer.render(text, |x,y,v| {
-            let i = (255f32 * v) as u8;
-            if i > 128 {
-                renderer.set_pixel(WIDTH - y as i32, x as i32, lcd::Color::rgb(i, i, i));
-            }
-        });
+            let mut text = String::from("Hi");
+            text.push_str("hi");
+            text.push_str(&cur_stack_height.to_string());
+            font_renderer.render(&text, |x,y,v| {
+                let i = (255f32 * v) as u8;
+                if i > 128 {
+                    renderer.set_pixel(WIDTH - y as i32, x as i32, lcd::Color::rgb(i, i, i));
+                }
+            });
         }
         last_tapped = tapped;
-
-
-
-
 
         // Timer
         let ms_per_frame = (1000 / FPS) as usize;
         loop {
             let cur_ms = system_clock::ticks();
-            if cur_ms - ms >= ms_per_frame { break; }
+            if cur_ms - ms >= ms_per_frame {
+                break;
+            }
         }
-
     }
-
-    
 }
 
 fn abs(x: f32) -> f32 {
@@ -407,17 +441,32 @@ fn hsv_color(hue: f32, s: f32, v: f32) -> lcd::Color {
     rgb.2 += m;
 
     lcd::Color::rgb(
-            (255f32 * rgb.0) as u8,
-            (255f32 * rgb.1) as u8,
-            (255f32 * rgb.2) as u8)
+        (255f32 * rgb.0) as u8,
+        (255f32 * rgb.1) as u8,
+        (255f32 * rgb.2) as u8,
+    )
 }
 
 fn draw_block(d: &mut Renderer, x: i32, y: i32, w: i32, h: i32, color: lcd::Color) {
     draw_rect(d, x, y, w, h, color);
-    for i in 0..h/15 {
+    for i in 0..h / 15 {
         draw_rect(d, x + w / 2 - 3, y + i * 15 + 5, 9, 7, color);
-        draw_line(d, x + w / 2 + 1, y + i * 15 + 5, x + w / 2 + 1, y + i * 15 + 11, color);
-        draw_line(d, x + w / 2 - 3, y + i * 15 + 8, x + w / 2 + 5, y + i * 15 + 8, color);
+        draw_line(
+            d,
+            x + w / 2 + 1,
+            y + i * 15 + 5,
+            x + w / 2 + 1,
+            y + i * 15 + 11,
+            color,
+        );
+        draw_line(
+            d,
+            x + w / 2 - 3,
+            y + i * 15 + 8,
+            x + w / 2 + 5,
+            y + i * 15 + 8,
+            color,
+        );
     }
 }
 
@@ -434,7 +483,9 @@ fn draw_line(d: &mut Renderer, x0: i32, y0: i32, x1: i32, y1: i32, color: lcd::C
         let d_x = x1 - x0;
         let d_y = y1 - y0;
         let mut d_err = d_y as f32 / d_x as f32;
-        if d_err < 0f32 { d_err = - d_err; }
+        if d_err < 0f32 {
+            d_err = -d_err;
+        }
         let mut error = 0f32;
         let mut y = y0;
         let mut sign_d_x = 1;
